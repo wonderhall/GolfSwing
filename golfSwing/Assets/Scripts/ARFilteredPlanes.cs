394484cd1d4510243c7a6fb92bbf1cf46bfadb6e
wonderhall <56276850+wonderhall@users.noreply.github.com>
2021-01-21 18:30:25 +0900

    office at 2021-01-21

---
+++ golfSwing/Assets/Scripts/ARFilteredPlanes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARFilteredPlanes : MonoBehaviour
+{
+    private ARPlaneManager arPlaneManager;
+
+    private List<ARPlane> aRPlanes;
+
+    private void OnEnable()
+    {
+        arPlaneManager = FindObjectOfType<ARPlaneManager>();
+        arPlaneManager.planesChanged += OnPlanesChanged;
+    }
+
+    private void OnDisable()
+    {
+        arPlaneManager.planesChanged -= OnPlanesChanged;
+    }
+
+    private void OnPlanesChanged(ARPlanesChangedEventArgs args)
+    {
+
+    }
+
+}
