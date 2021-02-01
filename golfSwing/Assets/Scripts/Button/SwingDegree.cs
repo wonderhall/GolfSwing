ff495a726644d4e3b69a8272a8dd533ef3fd68ee
wonderhall <56276850+wonderhall@users.noreply.github.com>
2021-02-01 18:19:55 +0900

    0201

--- golfSwing/Assets/Scripts/Button/SwingDegree.cs
+++ golfSwing/Assets/Scripts/Button/SwingDegree.cs
@@ -6,55 +6,87 @@ using TMPro;
 
 public class SwingDegree : MonoBehaviour
 {
+    //   [SerializeField]
+    //   private Transform m_target;
+    //   [SerializeField]
+    ////   private Transform m_origin;
+
+    //   private TextMeshProUGUI TextDeg;
+
+
+
+
+
+    //   // Update is called once per frame
+    //   void Update()
+    //   {
+
+    //       //      Vector3 direction = m_target.transform.position - m_origin.transform.position;
+    //       Vector3 direction = m_target.transform.position -this.gameObject.transform.position;
+
+    //       float angle = Mathf.Atan2(direction.y, direction.z) * Mathf.Rad2Deg;
+    //       if (angle < 0f) angle += 360f;
+    //       ////////////////
+
+    //       Debug.Log(angle);
+    //       TextDeg.text = $"{(int)angle}'";
+
+    //   }
+
+    private Transform m_target;
+    
     [SerializeField]
-    private Transform m_target;  
+    private Transform m_point;
+
+    //  private Transform m_target;
+    //   private Transform m_target = GameObject.FindWithTag("GolfClub_Base").transform;
+    //   private Transform m_origin;
+
     [SerializeField]
-    private Transform m_origin;
-    //[SerializeField]
-    //private GameObject m_target;
-    [SerializeField]
-    //private GameObject m_origin;
-    //[SerializeField]
     private TextMeshProUGUI TextDeg;
 
-    //private Vector3 origin;
-    //private Transform target;
+    float PositonY;
+    float PositonZ;
+    private void Start()
+    {
 
 
+        m_target = GameObject.FindWithTag("GolfClub_Base").transform;
+        //      m_target = player.transform.Find("Gun").gameObject;
+        //      GameObject.FindGameObjectWithTag("GolfClub_Base");
+        //    m_target = FindObjectOfType<Transform>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 origen = m_target.transform.position;
-        //Vector3 target = m_target.transform.position;
+ 
+        if (m_target.position.y > 0.75)
+        {
+            PositonY = 0.75f;
+        }
+        else PositonY = m_target.position.y;
+        if (m_target.position.z < -0.5)
+        {
+            PositonZ = m_target.position.z;
+
+        }
+        else
+            PositonZ = -0.5f;
 
 
-        ////      float angle = Mathf.Atan2(origin.y, target.x) * Mathf.Rad2Deg;
-        //Vector3 direction = target - origen;
 
-        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        //if (angle < 0f) angle += 360f;
+        m_point.transform.position= new Vector3(m_point.transform.position.x, PositonY, PositonZ);
+        //      Vector3 direction = m_target.transform.position - m_origin.transform.position;
+        Vector3 direction = m_target.transform.position - this.gameObject.transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.z) * Mathf.Rad2Deg;
+        if (angle < 0f) angle += 360f;
         ////////////////
 
-
-        //float angle = Vector3.Angle((m_target.transform.position - m_origin.transform.position), m_origin.transform.right);
-        //float angle2 = Vector3.Angle((m_target.transform.position - m_origin.transform.position), m_origin.transform.right);
-
-        //if (angle2 > 90)
-        //{
-        //    angle = 360 - angle;
-        //}
-        //float angle = Vector3.Angle((m_target.transform.position - m_origin.transform.position), m_origin.transform.right);
-
-        //float angle = Vector3.Angle(transform.position, m_target.position);
-        //Debug.Log(angle);
-
-        float angle = Vector3.Angle(m_origin.right, m_target.right);
-        Debug.Log(angle);
+   
         TextDeg.text = $"{(int)angle}'";
 
+
     }
-
-
-
 }
